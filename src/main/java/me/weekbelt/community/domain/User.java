230c2ca2d0b4975c76ec1571a3256a8168a210f5
joohyuk <vfrvfr4207@gmail.com>
2020-02-21 16:40:13 +0900

    Apply Spring Security + OAuth2

--- src/main/java/me/weekbelt/community/domain/User.java
+++ src/main/java/me/weekbelt/community/domain/User.java
@@ -3,6 +3,7 @@ package me.weekbelt.community.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.weekbelt.community.domain.enums.SocialType;
 import org.hibernate.annotations.CollectionId;
 
 import javax.persistence.*;
@@ -22,15 +23,21 @@ public class User implements Serializable {
     private String password;
     private String email;
 
+    private String principal;
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
     private LocalDateTime createdDate;
     private LocalDateTime updateDate;
 
     @Builder
-    public User(String name, String password, String email,
-                LocalDateTime createdDate, LocalDateTime updateDate) {
+    public User(String name, String password, String email, String principal,
+                SocialType socialType, LocalDateTime createdDate, LocalDateTime updateDate) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.principal = principal;
+        this.socialType = socialType;
         this.createdDate = createdDate;
         this.updateDate = updateDate;
     }
